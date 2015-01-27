1be71c571da14a5ee7898ced63e847409328c39c
Alexi Chow <alexipchow@gmail.com>
2015-01-26 21:09:43 -0500

    Revised file I/O

---
+++ GameDeals.py
@@ -0,0 +1,86 @@
+import smtplib
+import praw
+import time
+import io
+import requests
+import json
+from twython import Twython, TwythonError
+
+# Authentication for Twitter
+twitter = Twython("", "", "-", "")
+
+# PRAW initialization
+r = praw.Reddit('PRAW submission fetcher from r/gamedeals')
+
+# URL for Google URL Shortener
+gUrl = 'https://www.googleapis.com/urlshortener/v1/url'
+
+# Already posted links
+alreadyPosted = [line.strip() for line in open('PostedLinks.txt')]
+
+# Function for sending emails containing deal information
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+              subject, message,
+              login, password,
+              smtpserver='smtp.gmail.com:587'):
+    header  = 'From: %s\n' % from_addr
+    header += 'To: %s\n' % ','.join(to_addr_list)
+    header += 'Cc: %s\n' % ','.join(cc_addr_list)
+    header += 'Subject: %s\n\n' % subject
+    message = header + message
+ 
+    server = smtplib.SMTP(smtpserver)
+    server.starttls()
+    server.login(login,password)
+    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.quit()
+
+
+subreddit = r.get_subreddit('gamedeals')
+
+# Main execution loop
+# Checks new posts against already posted deals
+# Update already posted links and post new deals to Twitter
+while(True):
+    for submission in subreddit.get_new(limit=25):
+        if submission.id not in alreadyPosted:
+            
+            postTitle = submission.title
+            url = submission.url
+            redditURL = submission.short_link
+            
+            data = json.dumps({'longUrl': url})
+            r = requests.post(gUrl, data, headers={'Content-Type': 'application/json'})
+            j = json.loads(r.text)
+            sURL = j['id']
+            
+            if len(postTitle) > 115:
+                postTitle = postTitle[0:116]
+                postTitle += "-"
+              
+            try:
+                tStatus = ""+postTitle+" "+sURL
+                twitter.update_status(status=tStatus)
+                alreadyPosted.insert(0,submission.id)
+                del alreadyPosted[-1]
+            except TwythonError as e:
+                print(e)
+            
+            #loginN = "gmail account"
+            #passw = "gmail password"
+            
+            #sendemail(from_addr    = 'yourgmailaddress@gmail.com', 
+                      #to_addr_list = ['toaddress@gmail.com'],
+                      #cc_addr_list = [], 
+                      #subject      = postTitle, 
+                      #message      = ""+postTitle+" "+url+" "+redditURL, 
+                      #login        = loginN, 
+                      #password     = passw)
+	
+    #Overwrites existing file containing already posted deals with 25 most-recent postings
+    f = open('PostedLinks.txt', 'w')
+    for post in alreadyPosted:
+        f.write(""+post+"\n")
+    f.close()
+    time.sleep(900)
+            
---
+++ README.md
@@ -0,0 +1,32 @@
+# GameDeals
+Automated Twitter account that posts recent deals from reddit.com/r/gamedeals.
+
+## Dependencies
+
+Python 2.7.x  
+[PRAW Python Reddit Api Wrapper](https://praw.readthedocs.org/en/latest/)  
+[Twython Twitter API Wrapper](https://twython.readthedocs.org/en/latest/index.html)  
+[Requests HTTP Library](http://docs.python-requests.org/en/latest/)
+
+## Installation
+
+PRAW:
+`pip install praw`
+
+Twython:
+`pip install twython`
+
+Requests:
+`pip install requests`
+
+`git clone https://github.com/alexichow/GameDeals`
+
+## Usage
+
+This program requires developer API keys from Twitter to function. More information can be found here: https://apps.twitter.com/  
+Provide the necessary keys in `Line 10`.
+
+Simply running `python GameDeals.py` will start the script.
+
+
+
